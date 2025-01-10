d9a773a969c7743d73af4965d57fad1fa08a23a7
nitish1899 <nkword1899@gmail.com>
2025-01-10 13:42:30 +0530

    add pdf upload

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -16,7 +16,7 @@ export const upload = multer({
   storage: multer.memoryStorage(), // Use memory storage to avoid storing the file on disk
   limits: { fileSize: 5 * 1024 * 1024 }, // Limit file size to 5MB
   fileFilter: (req, file, cb) => {
-    if (['image/jpeg', 'image/png', 'image/jpg', 'image/pdf'].includes(file.mimetype)) {
+    if (['image/jpeg', 'image/png', 'image/jpg', 'application/pdf'].includes(file.mimetype)) {
       cb(null, true);
     } else {
       cb(new ApiError(400, 'Invalid file type. Please upload an image.'), false);
